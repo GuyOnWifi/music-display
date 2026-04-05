20d2799272c640bdcff3619906bbbcc2f5b6be9c
Eason Huang <me@easonhuang.dev>
2026-04-05 19:45:08 -0400

    initial setup

---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <Arduino.h> // Include the Arduino core library
+#include <Wire.h>    // Include the Wire library for I2C communication
+#include <u8g2.h>    // Download the U8g2 library
+// Create a U8g2 object, specifying the use of hardware I2C interface,
+// and setting the display model to SSD1306 with a resolution of 128x64 pixels
+void setup(void) {}
+void loop(void) {
+  // The program loop, which is empty and does not perform any operations here
+}
